d8ae46749bc38c3fba9fefc5ea8dc02ae920b564
monodread <mail@jonashummel.de>
2014-06-29 17:01:23 +0200

    latest indentation fixes

--- Tutorials/_essential/autostartExamples/autostart.py
+++ Tutorials/_essential/autostartExamples/autostart.py
@@ -4,11 +4,11 @@
 import OSC
 from OSC import OSCClient, OSCMessage
 from threading import Timer
-import Adafruit_BBIO.ADC as ADC
+import Adafruit_BBIO.GPIO as GPIO
 
 inPin = "P9_40" # connect LDR to this pin
-sendAddress = '127.0.0.1', 57120 # localhost, supercollider default port
-sensingPollRate = 0.05 # 0.05 = 20ms
+sendAddress = '127.0.0.1', 57120 # address to send to SuperCollider
+sensingPollRate = 0.05 # rate at which values will be read (0.05 = 20ms)
 
 def init_sensing_loop():
 	Timer(sensingPollRate, sense_and_send_values).start()
@@ -24,14 +24,14 @@ def sense_and_send_values():
 	except:
 		print "waiting for supercollider to become available"
 		pass
-	init_sensing_loop() # recursive call, keeps timer goingR
+	init_sensing_loop() # recursive call, keeps timer going
 
 # main:
 ADC.setup()
 client = OSCClient()
 client.connect( sendAddress )
-init_sensing_loop()
+init_sensing_loop() # init call to start the sensing loop
 
-try: 
-     while True: 
-         time.sleep(1)
+try:
+	while True:
+		time.sleep(1)
