b7bff3c7f714edb587da1d20b93832e85550b612
monodread <mail@jonashummel.de>
2014-06-29 16:35:34 +0200

    Update autostart.py

--- Tutorials/_essential/autostartExamples/autostart.py
+++ Tutorials/_essential/autostartExamples/autostart.py
@@ -1,14 +1,14 @@
-# Soundvase project - python script
-# - AIM: poll the value of a sensor and send as OSC to sclang e.g. '/shift 52'
+# autoStart.py
+# - a simple python script which repeatedly polls a photoresistor, passing this reading to SuperCollider for sonification
 
 import OSC
 from OSC import OSCClient, OSCMessage
 from threading import Timer
-import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.ADC as ADC
 
-inPin = "P9_40" # connect potentiometer to this pin
-sendAddress = '127.0.0.1', 57120 # address to send to SuperCollider
-sensingPollRate = 0.05 # rate at which values will be read (0.05 = 20ms)
+inPin = "P9_40" # connect LDR to this pin
+sendAddress = '127.0.0.1', 57120 # localhost, supercollider default port
+sensingPollRate = 0.05 # 0.05 = 20ms
 
 def init_sensing_loop():
 	Timer(sensingPollRate, sense_and_send_values).start()
@@ -16,7 +16,7 @@ def init_sensing_loop():
 def sense_and_send_values():
 	sensedValue = ADC.read(inPin)
 	msg = OSC.OSCMessage()
-	msg.setAddress('/shift')
+	msg.setAddress('/light')
 	msg.append(sensedValue)
 	print "sending locally to supercollider: '{0}', '{1}'".format(msg, client)
 	try:
@@ -24,21 +24,14 @@ def sense_and_send_values():
 	except:
 		print "waiting for supercollider to become available"
 		pass
-	init_sensing_loop() # recursive call, keeps timer going
+	init_sensing_loop() # recursive call, keeps timer goingR
 
 # main:
 ADC.setup()
 client = OSCClient()
 client.connect( sendAddress )
-init_sensing_loop() # init call to start the sensing loop
+init_sensing_loop()
 
-try:
-	while True:
-		time.sleep(1) 
-
-except KeyboardInterrupt:
-    print "\nClosing OSCServer."
-    pythonServer.close()
-    print "Waiting for Server-thread to finish"
-    st.join() ##!!!
-    print "Done"
+try: 
+     while True: 
+         time.sleep(1)
