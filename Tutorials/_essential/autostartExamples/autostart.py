903d366ebc4e960337c97170a46b06754a60211e
Graham Booth <graham.r.booth@gmail.com>
2014-06-29 23:02:44 +0800

    commit before pull

--- Tutorials/_essential/autostartExamples/autostart.py
+++ Tutorials/_essential/autostartExamples/autostart.py
@@ -11,14 +11,14 @@ sendAddress = '127.0.0.1', 57120 # localhost, supercollider default port
 sensingPollRate = 0.05 # 0.05 = 20ms
 
 def init_sensing_loop():
-        Timer(sensingPollRate, sense_and_send_values).start()
+	Timer(sensingPollRate, sense_and_send_values).start()
 
 def sense_and_send_values():
-        sensedValue = ADC.read("P9_40")
-        msg = OSC.OSCMessage()
-        msg.setAddress('/light')
-        msg.append(sensedValue)
-        print "sending locally to supercollider: '{0}', '{1}'".format(msg, client)
+	sensedValue = ADC.read("P9_40")
+	msg = OSC.OSCMessage()
+	msg.setAddress('/light')
+	msg.append(sensedValue)
+	print "sending locally to supercollider: '{0}', '{1}'".format(msg, client)
         try:
                 client.send ( msg )
         except:
