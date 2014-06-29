ba10f1485a1c15413c130370ff6e58b7804e2f9c
monodread <mail@jonashummel.de>
2014-06-29 16:25:25 +0200

    proofread and changed some lines

--- Tutorials/_essential/autostartExamples/autostart.py
+++ Tutorials/_essential/autostartExamples/autostart.py
@@ -1,37 +1,44 @@
-# autoStart.py
-# - a simple python script which repeatedly polls a photoresistor, passing this reading to SuperCollider for sonification
+# Soundvase project - python script
+# - AIM: poll the value of a sensor and send as OSC to sclang e.g. '/shift 52'
 
 import OSC
 from OSC import OSCClient, OSCMessage
-import threading
 from threading import Timer
-import Adafruit_BBIO.ADC as ADC
+import Adafruit_BBIO.GPIO as GPIO
 
-sendAddress = '127.0.0.1', 57120 # localhost, supercollider default port
-sensingPollRate = 0.05 # 0.05 = 20ms
+inPin = "P9_40" # connect potentiometer to this pin
+sendAddress = '127.0.0.1', 57120 # address to send to SuperCollider
+sensingPollRate = 0.05 # rate at which values will be read (0.05 = 20ms)
 
 def init_sensing_loop():
-        Timer(sensingPollRate, sense_and_send_values).start()
+	Timer(sensingPollRate, sense_and_send_values).start()
 
 def sense_and_send_values():
-        sensedValue = ADC.read("P9_40")
-        msg = OSC.OSCMessage()
-        msg.setAddress('/light')
-        msg.append(sensedValue)
-        print "sending locally to supercollider: '{0}', '{1}'".format(msg, client)
-        try:
-                client.send ( msg )
-        except:
-                print "waiting for supercollider to become available"
-                pass
-        init_sensing_loop() # recursive call, keeps timer goingR
+	sensedValue = ADC.read(inPin)
+	msg = OSC.OSCMessage()
+	msg.setAddress('/shift')
+	msg.append(sensedValue)
+	print "sending locally to supercollider: '{0}', '{1}'".format(msg, client)
+	try:
+		client.send ( msg )
+	except:
+		print "waiting for supercollider to become available"
+		pass
+	init_sensing_loop() # recursive call, keeps timer going
 
 # main:
-ADC.setup("P9_33")
+ADC.setup()
 client = OSCClient()
 client.connect( sendAddress )
-init_sensing_loop()
+init_sensing_loop() # init call to start the sensing loop
 
 try:
-     while True:
-         time.sleep(1)
+	while True:
+		time.sleep(1) 
+
+except KeyboardInterrupt:
+    print "\nClosing OSCServer."
+    pythonServer.close()
+    print "Waiting for Server-thread to finish"
+    st.join() ##!!!
+    print "Done"
